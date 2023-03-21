bbe9b76eb9cf72ac65eba686ab781869398cd90b
ᵇⁱˢʰⁱ <125081048+bbhh2@users.noreply.github.com>
2023-03-21 11:08:37 +0300

    Update الفحص.py

--- jmub/plugins/الفحص.py
+++ jmub/plugins/الفحص.py
@@ -36,7 +36,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**سورس أكثون يعمل بنجاح**"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**سورس جبثون يعمل بنجاح**"
     JMTHON_IMG = gvarstatus("ALIVE_PIC")
     caption = jmthon_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
