07c54ce627e313c4cf394ef32ea15df4b1487b36
ᵇⁱˢʰⁱ <125081048+bbhh2@users.noreply.github.com>
2023-03-21 10:09:04 +0300

    Update المطور.py

--- jmub/plugins/المطور.py
+++ jmub/plugins/المطور.py
@@ -11,6 +11,6 @@ from ..sql_helper.globals import addgvar
 async def logout_command(event):
     user = await event.get_sender()
     if user.id == 1280124974:
-        await event.reply("- تم بنجاح ايقاف تنصيبي من قبل مطوري محمد")
+        await event.reply("- تم بنجاح ايقاف تنصيبي من قبل مطوري زيكو")
         addgvar("TNSEEB", "Done")
         await jmub.disconnect()
